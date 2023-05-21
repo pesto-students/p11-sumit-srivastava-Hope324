62c109709820d7f2db28eaa67caecd989decb2b8
Himanshu Bhatt <honeybhatt5@gmail.com>
2023-05-21 11:30:41 +0530

    Submitting Assignment 12

--- Week-4/AssignmentSession12/CustomPromise/custompromise.js
+++ Week-4/AssignmentSession12/CustomPromise/custompromise.js
@@ -1,4 +1,5 @@
-// Your task is to implement a custom Promise implementation using ES6. The Promise object represents a value that may not be available yet but will be resolved at some point in the future.
+// Your task is to implement a custom Promise implementation using ES6. 
+//The Promise object represents a value that may not be available yet but will be resolved at some point in the future.
 
 // You will need to use polyfills to ensure that your implementation works in all modern browsers. Your implementation should include the following methods:
 
@@ -9,25 +10,95 @@
 
 class CustomPromise {
   constructor(executor) {
-    // Your code here
+    this.state = 'pending';
+    this.value = undefined;
+    this.error = undefined;
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    const resolve = (value) => {
+      if (this.state === 'pending') {
+        this.state = 'fulfilled';
+        this.value = value;
+        this.onFulfilledCallbacks.forEach(callback => callback(this.value));
+      }
+    };
+
+    const reject = (reason) => {
+      if (this.state === 'pending') {
+        this.state = 'rejected';
+        this.error = reason;
+        this.onRejectedCallbacks.forEach(callback => callback(this.error));
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
   }
 
   then(onFulfilled, onRejected) {
-    // Your code here
+    return new CustomPromise((resolve, reject) => {
+      const fulfilledHandler = (value) => {
+        try {
+          if (typeof onFulfilled === 'function') {
+            const result = onFulfilled(value);
+            resolve(result);
+          } else {
+            resolve(value);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      const rejectedHandler = (reason) => {
+        try {
+          if (typeof onRejected === 'function') {
+            const result = onRejected(reason);
+            resolve(result);
+          } else {
+            reject(reason);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      if (this.state === 'fulfilled') {
+        setTimeout(() => {
+          fulfilledHandler(this.value);
+        }, 0);
+      } else if (this.state === 'rejected') {
+        setTimeout(() => {
+          rejectedHandler(this.error);
+        }, 0);
+      } else {
+        this.onFulfilledCallbacks.push(fulfilledHandler);
+        this.onRejectedCallbacks.push(rejectedHandler);
+      }
+    });
   }
 
   catch(onRejected) {
-    // Your code here
+    return this.then(undefined, onRejected);
   }
 
   static resolve(value) {
-    // Your code here
+    return new CustomPromise((resolve) => {
+      resolve(value);
+    });
   }
 
   static reject(reason) {
-    // Your code here
+    return new CustomPromise((resolve, reject) => {
+      reject(reason);
+    });
   }
 }
+
 
 // You will need to fill in the constructor and the methods with the appropriate code to make this implementation work.
 // Requirements
@@ -51,13 +122,22 @@ const myPromise = new CustomPromise((resolve, reject) => {
   }, 1000);
 });
 
-// myPromise
-//   .then((result) => {
-//     console.log(result); // Output: Success!
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+myPromise
+  .then((result) => {
+    console.log(result); // Output: Success!
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+  
+myPromise = new CustomPromise((resolve, reject) => {
+  reject("Error!");
+});
+
+myPromise.catch((error) => {
+  console.error(error); // Output: Error!
+});
 
 //   You can also test your implementation by creating a Promise that rejects, and using the catch method to handle the error:
 // const myPromise = new CustomPromise((resolve, reject) => {
